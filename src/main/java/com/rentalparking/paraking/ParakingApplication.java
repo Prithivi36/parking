5c52e797c3b143ef0ece3ffda1c3224da9fc50ba
Prithivi36 <prithivipappian@gmail.com>
2024-10-19 22:36:37 +0530

    Parking-Project-Backend

---
+++ src/main/java/com/rentalparking/paraking/ParakingApplication.java
@@ -0,0 +1,13 @@
+package com.rentalparking.paraking;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ParakingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ParakingApplication.class, args);
+	}
+
+}
