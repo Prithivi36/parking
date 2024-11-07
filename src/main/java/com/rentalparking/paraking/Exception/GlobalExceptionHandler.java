354aff2c20cb1cc33b6cb7bb4ac6f861481d1617
Prithivi36 <prithivipappian@gmail.com>
2024-11-07 22:20:48 +0530

    Exceptional handling fix

--- src/main/java/com/rentalparking/paraking/Exception/GlobalExceptionHandler.java
+++ src/main/java/com/rentalparking/paraking/Exception/GlobalExceptionHandler.java
@@ -3,12 +3,14 @@ package com.rentalparking.paraking.Exception;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.context.request.WebRequest;
 
 import java.time.LocalDateTime;
 
 @ControllerAdvice
 public class GlobalExceptionHandler {
+    @ExceptionHandler(ApiException.class)
     public ResponseEntity<ErrorDetails> handleException(ApiException exception, WebRequest webRequest){
         ErrorDetails erd = new ErrorDetails(
                 LocalDateTime.now(),
